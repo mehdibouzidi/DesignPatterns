b3574b991955848ff34f35a9a16a1a4597a5006b
Mehdi BOUZIDI <mehdi.bouzidi.92@gmail.com>
2022-12-23 13:06:04 +0100

    Functional Interface Builder

---
+++ src/main/java/me/mbouzidi/designpatterns/creational/builder/functionalinterface/Car.java
@@ -0,0 +1,73 @@
+package me.mbouzidi.designpatterns.creational.builder.functionalinterface;
+
+/**
+ * This builder will oblige you to build the object in a specific order
+ * */
+public class Car {
+    private int seats;
+    private String engine; //might be an object of type "Engine"
+    private String tripComputer; //might be an object of type "TripComputer"
+    private String GPS; //might be an object of type "GPS"
+
+    public Car() {
+    }
+
+    public Car(int seats, String engine, String tripComputer, String GPS) {
+        this.seats = seats;
+        this.engine = engine;
+        this.tripComputer = tripComputer;
+        this.GPS = GPS;
+    }
+
+
+    public static Seats buildCar() {
+        return seats -> engine -> tripComputer -> gps -> new Car(seats, engine, tripComputer,gps);
+    }
+    @FunctionalInterface
+    public interface Seats{
+        Engine seats(int seats);
+    }
+
+    @FunctionalInterface
+    public interface Engine{
+        TripComputer engine(String engine);
+        default TripComputer defaultEngine() {
+            return engine("NORMAL ENGINE");
+        }
+    }
+
+    @FunctionalInterface
+    public interface TripComputer{
+        GPS tripComputer(String tripComputer);
+    }
+
+    @FunctionalInterface
+    public interface GPS{
+        Car gps(String GPS);
+    }
+
+    public static void main(String[] args) {
+        Car sportCar = Car.buildCar()
+                .seats(3)
+                .engine("Sport Engine")
+                .tripComputer("ULTRA Computer")
+                .gps("NEW GARMIN GPS");
+        System.out.println(sportCar);
+        Car normaleCar = Car.buildCar()
+                .seats(3)
+                .defaultEngine()
+                .tripComputer("ULTRA Computer")
+                .gps("NEW GARMIN GPS");
+        System.out.println(normaleCar);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "seats=" + seats +
+                ", engine='" + engine + '\'' +
+                ", tripComputer='" + tripComputer + '\'' +
+                ", GPS='" + GPS + '\'' +
+                '}';
+    }
+}
