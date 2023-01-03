903e4f3334d9f13de21e9c3fa10739c681f052dc
Mehdi BOUZIDI <mehdi.bouzidi.92@gmail.com>
2023-01-03 18:45:28 +0100

    Bridge Description

--- src/main/java/me/mbouzidi/designpatterns/structural/bridge/BridgeMain.java
+++ src/main/java/me/mbouzidi/designpatterns/structural/bridge/BridgeMain.java
@@ -1,5 +1,14 @@
 package me.mbouzidi.designpatterns.structural.bridge;
 
+/**
+ * Applicability:
+ * - Use the Bridge pattern when you want to divide and organize a monolithic class that has several variants of some functionality (for example, if the class can work with various database servers).
+ * - Use the pattern when you need to extend a class in several orthogonal (independent) dimensions.
+ * -  Use the Bridge if you need to be able to switch implementations at runtime.
+ *
+ * Source 1: https://refactoring.guru/design-patterns/bridge
+ * Source 2: https://refactoring.guru/design-patterns/bridge/java/example
+ * */
 public class BridgeMain {
     public static void main(String[] args) {
         testDevice(new Tv());
